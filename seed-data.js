a2e026f423faa8b790a2794c0a1f6e70a4203e35
j-goodman <johncurtisgoodman@gmail.com>
2026-04-30 20:25:27 -0400

    Add seed data.

---
+++ seed-data.js
@@ -0,0 +1,24 @@
+const seedData = [
+    {
+        "body": "Quoth the raven, nevermore.",
+        "author": "Poe"
+    },
+    {
+        "body": "What happens to a dream deferred?",
+        "author": "Hughes"
+    },
+    {
+        "body": "Two roads diverged in a yellow wood.",
+        "author": "Frost"
+    },
+    {
+        "body": "Because I could not stop for Death, he kindly stopped for me.",
+        "author": "Dickinson"
+    },
+    {
+        "body": "I wandered lonely as a cloud.",
+        "author": "Wordsworth"
+    }
+]
+
+export default seedData
